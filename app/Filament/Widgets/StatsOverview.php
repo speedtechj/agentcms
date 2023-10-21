a71c08b3ce25373dfc599a480bebaedd922df977
speedtechj <arbiasjr@gmail.com>
2023-10-21 12:09:36 -0600

    update

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -11,7 +11,7 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total Box', Booking::where('is_pickup', 0)
+            Stat::make('Total Box', Booking::where('is_pickup', 1)
             ->where('booking_date', now()->format('Y-m-d'))
             ->where('agent_id', auth()->user()->id)
             ->count())
@@ -19,7 +19,7 @@ class StatsOverview extends BaseWidget
             ->descriptionIcon('heroicon-m-arrow-trending-up')
             ->chart([7, 2, 10, 3, 15, 4, 17])
             ->color('success'),
-            Stat::make('Total Box', Booking::where('is_pickup', 1)
+            Stat::make('Total Box', Booking::where('is_pickup', 0)
             ->where('booking_date', now()->format('Y-m-d'))
             ->where('agent_id', auth()->user()->id)
             ->count())
